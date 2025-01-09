2859c9aacc5e4ebacc2d271e3e33d03a753eb425
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-01-09 08:17:49 -0600

    Added react-leaflet

--- Booking-Hotel-App/src/components/Map/Map.jsx
+++ Booking-Hotel-App/src/components/Map/Map.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import { useHotels } from "../Context/HotelsProvider";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function Map() {
-    const { hotels, isLoading } = useHotels();
+  const { hotels, isLoading } = useHotels();
+  const [mapCenter, setMapCenter] = useState([20, 4]);
+
   return (
     <div className="mapContainer">
-      Map
+      <MapContainer className="map" center={mapCenter} zoom={6} scrollWheelZoom={true}>
+      <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+      />
+        {hotels.map((item) => {
+          return (
+            <Marker key={item.id} position={[item.latitude, item.longitude]}>
+              <Popup>
+                {item.name}
+              </Popup>
+            </Marker>
+          )
+        })}
+      </MapContainer>
     </div>
   )
 }
