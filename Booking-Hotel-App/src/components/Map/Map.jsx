766972b670b62d337f230313910e3c90e8324438
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-01-09 08:58:54 -0600

    Completed Map Component

--- Booking-Hotel-App/src/components/Map/Map.jsx
+++ Booking-Hotel-App/src/components/Map/Map.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useHotels } from "../Context/HotelsProvider";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { useSearchParams } from "react-router-dom";
 
 function Map() {
   const { hotels, isLoading } = useHotels();
   const [mapCenter, setMapCenter] = useState([20, 4]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  useEffect(() => {
+    if (lat && lng) {
+      setMapCenter([lat, lng]);
+    }
+  }, [lat, lng])
 
   return (
     <div className="mapContainer">
@@ -13,6 +24,7 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
       />
+      <ChangeCenter position={mapCenter} />
         {hotels.map((item) => {
           return (
             <Marker key={item.id} position={[item.latitude, item.longitude]}>
@@ -28,3 +40,11 @@ function Map() {
 }
 
 export default Map
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+
+  map.setView(position);
+
+  return null;
+}
